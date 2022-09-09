c69b5c247fc4bce7a6d44157fdb09d46da1cd439
Musya <abashka1994@gmail.com>
2022-09-09 20:10:03 +0300

    add components

--- src/components/appBanner/AppBanner.js
+++ src/components/appBanner/AppBanner.js
@@ -1,13 +1,16 @@
-const ComicsList = () => {
+
+
+const AppBanner = () => {
   return (
     <div class="app__banner">
       {/* <img src="img/Avengers.png" alt="Avengers"> */}
       <div class="app__banner-text">
-        New comics every week!<br>
+        New comics every week!
+        {/* <br> */}
         Stay tuned!
       </div>
       {/* <img src="img/Avengers_logo.png" alt="Avengers logo"> */}
     </div>
   )
 }
-export default ComicsList
+export default AppBanner;
