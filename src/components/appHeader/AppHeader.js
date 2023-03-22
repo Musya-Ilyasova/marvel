c0b5efece48e484c7dfe18261930512c53535d2b
Musya <musya.ilyasova@getorca.app>
2023-03-22 15:13:09 +0300

    fixed menu

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -4,7 +4,7 @@ const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <Link to="/">
+        <Link to="/marvel">
           <span>Marvel </span>
           information portal
         </Link>
